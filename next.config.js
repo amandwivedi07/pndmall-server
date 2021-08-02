f34ad7534573064c2f960a22d1c4a71b7bc42c33
Aman <diligentdwivedi@gmail.com>
2021-08-02 16:03:57 +0530

    first commit

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    env: {
+        "BASE_URL": "http://localhost:3000",
+        "MONGODB_URL": "mongodb+srv://pndmall:Pndmall123@@pndmall.t9rcb.mongodb.net/pndmall?retryWrites=true&w=majority",
+        "ACCESS_TOKEN_SECRET": "4Vy6AzkbKJzCQbD58acQy97jMYzHnTa2wgWxpmzbmFCpNxEYGK",
+        "REFRESH_TOKEN_SECRET": "fxW2MhG9xXxahxH8ApfTr7pcr2nwgpEHwDjvLSxccZLQhWV3wYHRjFtJvfMRUNjh6NtX6mxHH8L4nwb8em95dUrZJBNUSy4Uw2Gp",
+        "PAYPAL_CLIENT_ID": "YOUR_PAYPAL_CLIENT_ID",
+        "CLOUD_UPDATE_PRESET": "pndmall_store",
+        "CLOUD_NAME": "pndmall",
+        "CLOUD_API": "https://api.cloudinary.com/v1_1/pndmall/image/upload"
+    }
+  }
